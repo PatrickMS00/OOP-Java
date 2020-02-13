052e67e9bb8f7d5293adf087afe541af1a219e91
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2020-02-13 12:28:21 -0500

    upload new JavaSwing Code

---
+++ JavaConsole/40 - Runtime vs. checked Exceptions/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+ 
+    public static void main(String[] args) {
+        try { 
+        // Null pointer exception ....
+        String text = null;
+         
+        System.out.println(text.length());
+         
+        // Arithmetic exception ... (divide by zero)
+        int value = 7/0;
+ 
+        // You can actually handle RuntimeExceptions if you want to;
+        // for example, here we handle an ArrayIndexOutOfBoundsException
+        String[] texts = { "one", "two", "three" };
+ 
+        
+            System.out.println(texts[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
